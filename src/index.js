f863bd8871f5083c57c2876369dcca5576805cd4
ThiagoJoseSousa <imigrationstudy@gmail.com>
2023-11-19 16:50:50 -0300

    responsive layout is ok

--- src/index.js
+++ src/index.js
@@ -5,17 +5,20 @@ import debounce from './helpers/debounce.js';
 
 const appContainer = $("#AppContainer");
 
-const c= $('<canvas id="graph"></canvas>');
+const c= $('<canvas id="graph" class="m-4"></canvas>');
 
-let header = $('<header></header>')
+let header = $('<header class="bg-dark"></header>')
 appContainer.append(header)
 
-const displaySearchSection = () => {
 
-    const searchSection = $("<section id='search-section'></section>")
-    const searchTitle = $("<h2 class='fw-bold'>Search for a coin</h2>")
-    const searchInput = $("<input type='text' class='container-fluid rounded fs-2'></input>"); 
-    const trendingTitle = $("<h2 class='fw-bold'>Trending coins</h2>")
+const displaySearchSection = () => {
+    const responsiveContainer = $('<div class="container"></div>');
+    const rowContainer = $('<div class="row"></div>')
+
+    const searchSection = $("<section id='search-section' class='col-md-8 col-xxl-12 m-auto text-center'></section>")
+    const searchTitle = $("<h2 class='fw-bold fs-3 mt-5'>Search for a coin</h2>")
+    const searchInput = $("<input type='text' class='container-fluid rounded fs-3 text-center'></input>"); 
+    const trendingTitle = $("<h2 class='fw-bold fs-3 mt-5 mb-4'>Trending coins</h2>");
 
 
     searchTitle.appendTo(searchSection)
@@ -23,8 +26,12 @@ const displaySearchSection = () => {
     searchInput.on('input', homeStore.setQuery)
     trendingTitle.appendTo(searchSection)
 
-    searchSection.appendTo(appContainer)
-
+    searchSection.appendTo(rowContainer);
+    
+    rowContainer.appendTo(responsiveContainer)
+    
+    responsiveContainer.appendTo(appContainer)
+    
     homeStore.updateLinks()
 }
 // make a new div called wrapper, sum the strings and change html()
@@ -64,7 +71,7 @@ const homeStore = {
         image:  item.large,
         id: item.id,
         priceBtc : coin.item.price_btc.toFixed(10),
-        priceUsd : (coin.item.price_btc * btcPrice).toFixed(10)
+        priceUsd : (coin.item.price_btc * btcPrice).toFixed(4)
     }
     })
     console.log(coins)
@@ -106,17 +113,17 @@ const homeStore = {
     createLinkCoins : () => {
         homeStore.linksCoins = homeStore.storedCoins.map(
         coin => {
-            const coinLink =$("<div class='border-top search-result'></div>")
-            const coinInfo = `<img src='${coin.image}' />
-            <a href='#${coin.id}'>${coin.name}</a>`;
+            const coinLink =$("<div class='border-bottom search-result d-flex align-items-center flex-column flex-sm-row text-start'></div>")
+            const coinInfo = `<img src='${coin.image}' class="py-4 px-4" width='100px' height='100px'/>
+            <a href='#${coin.id}' class='d-block me-sm-auto text-reset text-decoration-none fs-4 text-sm-start text-center'>${coin.name}</a>`;
             
             coinLink.append(coinInfo);
             
             if (!coin.priceBtc){
                 return coinLink
             } else{
-                const coinConversion = `<i class="fa-brands fa-bitcoin" style="color: #ff8000;"></i> 
-                ${coin.priceBtc} ${coin.priceUsd}`
+                const coinConversion = `<div><div class="fs-5" style="white-space:nowrap"><i class="fa-brands fa-bitcoin fs-5" style="color: #ff8000;"></i> 
+                ${coin.priceBtc}</div> <div  class="text-center text-sm-end"><span class="fs-6 fw-lighter">(${coin.priceUsd} USD)</div>`
                 coinLink.append(coinConversion)
                 return coinLink
             }
@@ -128,6 +135,7 @@ const homeStore = {
         homeStore.createLinkCoins();
         //trending coins or search results
         $('#search-section').append(homeStore.linksCoins);
+
     }
 
 }
@@ -163,17 +171,16 @@ const HashChange= () => {
 
     let hash = window.location.hash;
     console.log(hash)        
-    $('#search-section').remove()
+    $('.container').remove()
     if( hash.startsWith("#") && hash.length>2 ){
         hash = hash.replace( "#" , "" );
         console.log(hash)
         
-        header.html('<h1 class="py-3 bg-dark text-light text-center text-uppercase fs-2 fw-bold"><a href="#"><i class="fa-solid fa-angle-left"></i></a>Coiner!</h1>')
+        header.html('<div class="container"><div class="col-xl-8 mx-auto position-relative"><h1 class="py-3 text-light text-uppercase fs-2 fw-bold row"> <a href="#" class="text-reset col-xl-8 position-absolute start-0 text-start"><i class="fa-solid fa-angle-left "></i></a><span class="text-center fs-3">Coiner!</span></h1></div></div>')
         fetchMarket(hash)
         
     } else {
-        $('#coin-info').remove()
-        header.html('<h1 class="py-3 bg-dark text-light text-center text-uppercase fs-2 fw-bold">Coiner!</h1>')
+        header.html('<h1 class="py-3 text-light text-center text-uppercase fs-3 fw-bold">Coiner!</h1>')
         displaySearchSection()
     }
     console.log(homeStore.linksCoins)
@@ -188,6 +195,12 @@ let chart;
 
 const displayGraph = async () => {
     const data = homeStore.graphData;
+    const skipData= Math.floor(data.length/10);
+    let filteredData=[];
+    for (let i=0; i<data.length-1; i+=skipData){
+        filteredData.push(data[i]);
+    }
+
     if (chart) {
     chart.destroy()
     }
@@ -197,13 +210,16 @@ const displayGraph = async () => {
         type: 'line',
         
         data: {
-          labels: data.map(row => row.date),
+          labels: filteredData.map(row => row.date),
           datasets: [
             {
-              label: 'Price',
-              data: data.map(row => row.price),
+              label: 'USD',            
+              data: filteredData.map(row => row.price),
               fill: 'origin',
-              lineTension: 0.4
+              lineTension: 0.5,
+              pointHitRadius: 30,
+              backgroundColor:'rgba(221,232,179,0.5)',
+              borderColor:'rgba(89,140,88 ,1)'
             },
           ]
         },
@@ -212,7 +228,23 @@ const displayGraph = async () => {
                 filler: {
                     propagate: true
                 }
-            }
+            },
+            scales: {
+                y: {
+                  ticks: {
+                    font: {
+                      size: 12,
+                    }
+                  }
+                },
+                x: {
+                    ticks: {
+                      font: {
+                        size: 12,
+                      }
+                    }
+                  }
+              },
         }
       }
     );
@@ -224,45 +256,51 @@ const displayCoinData = () => {
 
     const {data} = homeStore;
     console.log(data)
-    appContainer.append(c);
-    displayGraph()
-
-    const coinWrapper = $('<section id="coin-info"></section>')
     
-    const coinTitle = $(`<div id="coin-title">
-    <h2>${data.name} ${data.symbol}</h2>
-    <img src=${data.image.large} />
+
+    const responsiveContainer = $('<div class="container"></div>');
+    const rowContainer = $('<div class="row"></div>')
+
+    const coinWrapper = $('<section id="coin-info" class="col-sm-8 col-xxl-12 m-auto"></section>')
+    
+    const coinTitle = $(`<div id="coin-title" class="text-center ">
+    <img src=${data.image.large} class="py-4 px-4 mt-4" width = '125px' height='125px'/>
+    <h2 class="fw-bold fs-3">${data.name} <span class="text-uppercase">(${data.symbol})<span/></h2>
     </div>`);
 
     const coinData = $(`<div id="coin-details">
-    <div>
-        <h4>Market cap rank</h4>
-        <span>${data.market_data.market_cap_rank}</span>
+    <h2 class="fw-bold fs-3 text-center my-4">Details</h2>
+    <div class="d-flex justify-content-sm-between flex-column flex-sm-row align-items-center border-bottom py-3">
+        <h4 class="fs-5 fw-bold mb-0">Market cap rank</h4>
+        <span class="fs-5">$${data.market_data.market_cap_rank}</span>
     </div>
-    <div>
-        <h4>24h high</h4>
-        <span>${data.market_data.high_24h.usd}</span>
+    <div class="d-flex justify-content-sm-between flex-column flex-sm-row align-items-center border-bottom py-3">
+        <h4 class="fs-5 fw-bold mb-0">24h high</h4>
+        <span class="fs-5">$${data.market_data.high_24h.usd}</span>
     </div>
-    <div>
-        <h4>24h low</h4>
-        <span>${data.market_data.low_24h.usd}</span>
+    <div class="d-flex justify-content-sm-between flex-column flex-sm-row align-items-center border-bottom py-3">
+        <h4 class="fs-5 fw-bold mb-0">24h low</h4>
+        <span class="fs-5">$${data.market_data.low_24h.usd}</span>
     </div>
-    <div>
-        <h4>Circulating supply</h4>
-        <span>${data.market_data.circulating_supply}</span>
+    <div class="d-flex justify-content-sm-between flex-column flex-sm-row align-items-center border-bottom py-3">
+        <h4 class="fs-5 fw-bold mb-0">Circulating supply</h4>
+        <span class="fs-5">$${data.market_data.circulating_supply}</span>
     </div>
-    <div>
-        <h4>Current price</h4>
-        <span>${data.market_data.current_price.usd}</span>
+    <div class="d-flex justify-content-sm-between flex-column flex-sm-row align-items-center border-bottom py-3">
+        <h4 class="fs-5 fw-bold mb-0">Current price</h4>
+        <span class="fs-5">$${data.market_data.current_price.usd}</span>
     </div>
-    <div>
-        <h4>1y change</h4>
-        <span>${data.market_data.price_change_percentage_1y.toFixed(2)}%</span>
+    <div class="d-flex justify-content-sm-between flex-column flex-sm-row align-items-center border-bottom py-3">
+        <h4 class="fs-5 fw-bold mb-0">1y change</h4>
+        <span class="fs-5">${data.market_data.price_change_percentage_1y.toFixed(2)}%</span>
     </div> 
     </div>`)
 
-    coinWrapper.appendTo(appContainer)
+    responsiveContainer.appendTo(appContainer)
+    rowContainer.appendTo(responsiveContainer)
+    coinWrapper.appendTo(rowContainer)
     coinTitle.appendTo(coinWrapper)
     c.appendTo(coinWrapper)
+    displayGraph()
     coinData.appendTo(coinWrapper)
 }
